34f19e64235ade38f48c0fa66c993e2ed0f0ea5e
weiyin-lu <weiyin2002@outlook.com>
2023-11-04 19:43:23 +0800

    swagger环境搭建

---
+++ src/main/java/online/weiyin/staffsync/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package online.weiyin.staffsync.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @ClassName SwaggerConfig
+ * @Description swagger配置类
+ * @Version 1.0.0
+ * @Time 2023/11/04 下午 07:40
+ * @Author 卢子昂
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().build();
+    }
+}
